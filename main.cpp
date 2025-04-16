a38ffce6143014c88c61c86fb29c04b27efc2caf
jamirlemos <jamir.lemos@gmail.com>
2025-04-16 23:21:21 +0000

    primeiro codigo

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Importa biblioteca de entrada e saída
+
+int main() {
+    std::cout << "Olá, mundo!" << std::endl; // Exibe mensagem no terminal
+    return 0; // Encerra o programa com sucesso
+}
